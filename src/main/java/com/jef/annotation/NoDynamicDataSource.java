03a59da0837af4f879b9e0ca2a0bbf26cfe2704e
jef.tu@foxmail.com <loveblog@520>
2021-03-29 14:51:56 +0800

    补充读写分离思路

---
+++ src/main/java/com/jef/annotation/NoDynamicDataSource.java
@@ -0,0 +1,18 @@
+package com.jef.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 添加注解跳过动态数据源切换
+ * @author Jef
+ * @date 2020/03/29
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface NoDynamicDataSource {
+}
